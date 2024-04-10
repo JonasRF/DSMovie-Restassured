19238f763e9c57f9d99686b638443de876ed2539
JonasRF <jonasribeiro41@yahoo.com.br>
2024-04-09 23:03:03 -0300

    testing controllers findAll and findById

--- src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -13,6 +13,7 @@ public class MovieControllerRA {
 
 	private String clientusername, clientpassword, adminusername, adminpassword;
 	private String clientToken, adminToken, invalidToken;
+	private String titleName;
 	private Long existingMovieId, nonExistingMovietId;
 
 	@BeforeEach
@@ -22,6 +23,7 @@ public class MovieControllerRA {
 		adminpassword = "123456";
 		clientusername = "alex@gmail.com";
 		clientpassword = "123456";
+		titleName = "Guerra";
 
 		clientToken = TokenUtil.obtainAccessToken(clientusername, clientpassword);
 		adminToken = TokenUtil.obtainAccessToken(adminusername, adminpassword);
@@ -39,15 +41,43 @@ public class MovieControllerRA {
 	}
 	
 	@Test
-	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {		
+	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {
+		given()
+				.get("/movies?title={titleName}", titleName)
+				.then()
+				.statusCode(200)
+				.body("content.id[0]", is(11))
+				.body("content.title[0]", equalTo("Star Wars: A Guerra dos Clones"))
+				.body("content.score[0]", is(0.0F))
+				.body("content.count[0]", is(0))
+				.body("content.image[0]", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/uK15I3sGd8AudO9z6J6vi0HH1UU.jpg"));
 	}
 	
 	@Test
-	public void findByIdShouldReturnMovieWhenIdExists() {		
+	public void findByIdShouldReturnMovieWhenIdExists() {
+		existingMovieId = 3L;
+
+		given()
+				.get("/movies/{id}", existingMovieId)
+				.then()
+				.statusCode(200)
+				.body("id", is(3))
+				.body("title", equalTo("O Espetacular Homem-Aranha 2: A Ameaça de Electro"))
+				.body("score", is(0.0F))
+				.body("count", is(0))
+				.body("image", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/u7SeO6Y42P7VCTWLhpnL96cyOqd.jpg"));
 	}
 	
 	@Test
-	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {	
+	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {
+		nonExistingMovietId = 50L;
+
+		given()
+				.get("/movies/{id}", nonExistingMovietId)
+				.then()
+				.statusCode(404)
+				.body("error", equalTo("Recurso não encontrado"))
+				.body("status", equalTo(404));
 	}
 	
 	@Test
